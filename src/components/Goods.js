71805f42b874a8a7150c3e90e6f9f5f9ec3403c8
Anastasia <polushkina@tochkak.ru>
2018-07-20 21:18:30 +0300

    buttons_switch

--- src/components/Goods.js
+++ src/components/Goods.js
@@ -3,7 +3,6 @@ import '../App.css';
 import './Fish.js';
 import {itemAddAction, itemRemoveAction} from '../actions/Actions.js';
 import {connect} from 'react-redux';
-import Button from './Button.js';
 
 const mapStateToProps = (state) => ({
   data: state.data,
@@ -18,22 +17,22 @@ const mapDispatchToProps = (dispatch) => ({
 const Goods = ({data, itemCount, itemAdd, itemRemove}) => {
   const itemInfo = data.map((item) => 
     <div className="item" key = {item.id}>
-      <h1 className="item-name">{item.name}</h1>
-      <div className="item-info">
-        <div className="item-info-img">
+      <h1 className="item__name">{item.name}</h1>
+      <div className="item__info">
+        <div className="item__info-img">
           <img src = {item.imgUrl} alt='Рыбка' />
         </div>
-        <div className="item-info-data">
+        <div className="item__info-data">
           <h2>Цена</h2>
           <p>{item.price} р.</p>
-          {/*<img className="button-img" src="http://bs.barcod.ru/upload/iblock/444/444e9b9d7400e141def517a2b9b5030f.png" onClick={() => itemAdd(item.price, item.id)} alt='Добавить в корзину' />
-          <div className="buttons"> 
+          {(itemCount[item.id] < 1) ?
+          <img className="item__button_img" src="http://bs.barcod.ru/upload/iblock/444/444e9b9d7400e141def517a2b9b5030f.png" onClick={() => itemAdd(item.price, item.id)} alt='Добавить в корзину' /> : null}
+          {(itemCount[item.id] > 0) ?
+          <div className="item__buttons"> 
             <button onClick={() => itemRemove(item.price, item.id)}>-</button>
             <div>{itemCount[item.id]}</div>
             <button onClick={() => itemAdd(item.price, item.id)}>+</button>
-          </div>
-          */}
-          <Button />
+          </div> : null}
         </div>
       </div>
     </div>
